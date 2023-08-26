923c3bdcfc2dfab8e2eff8af373262eacc1d2537
Mark Warren <thofnas1@gmail.com>
2023-08-26 15:11:51 +0300

    Simple animations

--- Assets/_Scripts/Unit.cs
+++ Assets/_Scripts/Unit.cs
@@ -3,9 +3,14 @@ using UnityEngine;
 public class Unit : MonoBehaviour
 {
     private const float TOLERANCE = 0.1f;
-    private const float MOVE_SPEED = 4;
+    private const float MOVE_SPEED = 4f;
+    private const float ROTATE_SPEED = 10f;
+
+    [SerializeField] private Animator _unitAnimator;
     
     private Vector3 _targetPosition;
+    
+    private static readonly int IsWalking = Animator.StringToHash(nameof(IsWalking));
 
     private void Update()
     {
@@ -15,11 +20,15 @@ public class Unit : MonoBehaviour
         if (Vector3.Distance(transform.position, _targetPosition) <= TOLERANCE)
         {
             transform.position = _targetPosition;
+            _unitAnimator.SetBool(IsWalking, false);
             return;
         }
 
         Vector3 moveDirection = (_targetPosition - transform.position).normalized;
         transform.position += moveDirection * (Time.deltaTime * MOVE_SPEED);
+        transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * ROTATE_SPEED);
+        
+        _unitAnimator.SetBool(IsWalking, true);
     }
 
     private void Move(Vector3 targetPosition)
