0639cad7fbb15da7b448f546713a7df5bcfd1201
Mark Warren <thofnas1@gmail.com>
2023-09-10 17:34:24 +0300

    Grenade visuals

--- Assets/_Scripts/GrenadeProjectile.cs
+++ Assets/_Scripts/GrenadeProjectile.cs
@@ -7,16 +7,29 @@ public class GrenadeProjectile : MonoBehaviour
     private const float MOVE_SPEED = 15f;
     private const float DAMAGE_RADIUS = 4f;
     private const double TOLERANCE = 0.2f;
-    
+    private const float MAX_HEIGHT = 4f;
+
+    public static event EventHandler OnAnyGrenadeExploded;
+
+    [SerializeField] private Transform _grenadeExplodeParticles;
+    [SerializeField] private TrailRenderer _trailRenderer;
+    [SerializeField] private AnimationCurve _arcYAnimationCurve;
     private Action _onGrenadeBehaviorComplete;
     private Vector3 _targetPosition;
+    private float _totalDistance;
+    private Vector3 _positionXZ;
 
     private void Update()
     {
-        Vector3 moveDir = (_targetPosition - transform.position).normalized;
-        transform.position += moveDir * (MOVE_SPEED * Time.deltaTime);
+        Vector3 moveDir = (_targetPosition - _positionXZ).normalized;
+        _positionXZ += moveDir * (MOVE_SPEED * Time.deltaTime);
+        float distance = Vector3.Distance(_positionXZ, _targetPosition);
+        float distanceNormalized = Mathf.InverseLerp(_totalDistance, 0f, distance);
+        float positionY = _arcYAnimationCurve.Evaluate(distanceNormalized) * _totalDistance / MAX_HEIGHT;
 
-        if (!(Vector3.Distance(transform.position, _targetPosition) < TOLERANCE)) return;
+        transform.position = new Vector3(_positionXZ.x, positionY, _positionXZ.z);
+        
+        if (!(Vector3.Distance(_positionXZ, _targetPosition) < TOLERANCE)) return;
 
         Collider[] colliders = Physics.OverlapSphere(_targetPosition, DAMAGE_RADIUS);
 
@@ -28,13 +41,22 @@ public class GrenadeProjectile : MonoBehaviour
         }
 
         _onGrenadeBehaviorComplete();
+
+        Instantiate(_grenadeExplodeParticles, _targetPosition, Quaternion.identity);
+        
+        _trailRenderer.transform.SetParent(null);
         
         Destroy(gameObject);
+        
+        OnAnyGrenadeExploded?.Invoke(this, EventArgs.Empty);
     }
 
     public void Setup(GridPosition targetGridPosition, Action onGrenadeBehaviorComplete)
     {
         _onGrenadeBehaviorComplete = onGrenadeBehaviorComplete;
         _targetPosition = LevelGrid.Instance.GetWorldPosition(targetGridPosition);
+        _positionXZ = transform.position;
+        _positionXZ.y = 0;
+        _totalDistance = Vector3.Distance(_positionXZ, _targetPosition);
     }
 }
--- Assets/_Scripts/ScreenShakeActions.cs
+++ Assets/_Scripts/ScreenShakeActions.cs
@@ -1,3 +1,4 @@
+using System;
 using Actions;
 using UnityEngine;
 
@@ -6,15 +7,22 @@ public class ScreenShakeActions : MonoBehaviour
     private void Start()
     {
         ShootAction.OnAnyUnitShoot += ShootAction_OnAnyUnitShoot;
+        GrenadeProjectile.OnAnyGrenadeExploded += GrenadeProjectile_OnAnyGrenadeExploded; 
     }
-    
+
     private void OnDestroy()
     {
         ShootAction.OnAnyUnitShoot -= ShootAction_OnAnyUnitShoot;
+        GrenadeProjectile.OnAnyGrenadeExploded -= GrenadeProjectile_OnAnyGrenadeExploded;
     }
     
     private void ShootAction_OnAnyUnitShoot(object sender, ShootAction.UnitShootEventArgs e)
     {
         ScreenShake.Instance.Shake(2f);
+    }
+    
+    private void GrenadeProjectile_OnAnyGrenadeExploded(object sender, EventArgs e)
+    {
+        ScreenShake.Instance.Shake(5f);
     }
 }
